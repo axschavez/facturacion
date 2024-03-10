449f1312597ffa5a282b4f332d05f2e67d01439b
Alexis Chavez <alexischavezbarria1@gmail.com>
2024-03-09 23:41:41 -0300

    se agrego el login y la pantilla del nav

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+$alert = '';
+session_start();
+if (!empty($_SESSION['active'])) {
+    header('location: sistema/');
+}else {
+    if (!empty($_POST)) {
+        if (empty($_POST['usuario']) || empty($_POST['clave'])) {
+            $alert = 'Ingrese su usuario y su clave';
+        }else {
+            require_once "conexion.php";
+    
+            $user = $_POST['usuario'];
+            $pass = $_POST['clave'];
+    
+            $query = mysqli_query($conection,"SELECT * FROM usuario WHERE usuario = '$user' AND clave = '$pass'");
+            $result = mysqli_num_rows($query);
+    
+            if ($result > 0) {
+                $data = mysqli_fetch_array($query);
+                $_SESSION['active'] = true;
+                $_SESSION['idUser'] = $data['idusuario'];
+                $_SESSION['nombre'] = $data['idusuario'];
+                $_SESSION['email'] = $data['email'];
+                $_SESSION['user'] = $data['usuario'];
+                $_SESSION['rol'] = $data['rol'];
+    
+                header('location: sistema/');
+            }else {
+                $alert = 'El usuario o la clave son incorrectos';
+                session_destroy();
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <section id="container">
+        <form action="" method="post">
+            <h3>Iniciar Sesión</h3><br>
+            <img src="img/login.png" alt="Login"><br>
+
+            <input type="text" name="usuario" placeholder="Usuario">
+            <input type="password" name="clave" placeholder="Contraseña">
+            <div class="alert"><?php echo isset($alert) ? $alert : '';  ?></div>
+            <input type="submit" value="INGRESAR"><br>
+        </form>
+    </section>
+</body>
+</html>
