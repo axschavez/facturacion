61f91dec0e868efad2cb24537d4001e835b099ac
Alexis Chavez <alexischavezbarria1@gmail.com>
2024-03-11 00:03:41 -0300

    se ha creado el registro del usuario

--- conexion.php
+++ conexion.php
@@ -6,9 +6,4 @@
 
     $conection = @mysqli_connect($host, $user, $password, $db);
 
-    if (!$conection) {
-        echo " Error en la conexión";
-    }else {
-        echo "Conexión exitosa";
-    }
 ?>
